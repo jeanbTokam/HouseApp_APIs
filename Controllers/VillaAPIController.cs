a976b15735b9b16f3618eaa077bd8d612c2c1e38
jbtokam <jeanbTokam@github.com>
2023-12-09 00:18:17 -0500

    Added Post Request

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -12,19 +12,42 @@ namespace HouseApp_APIs.Controllers
     {
         // GET: api/VillaAPI - get all villas
         [HttpGet]
-        public IEnumerable<VillaDto> GetVillas()
+        public ActionResult <IEnumerable<VillaDto>> GetVillas()
         {
-            return VillaStore.VillaList;
+            return Ok(VillaStore.VillaList);
 
         }
 
 
         // GET: api/VillaAPI - get Villa by Id
         [HttpGet("id")]
-        public VillaDto GetVilla(int id)
+        public ActionResult <VillaDto> GetVilla(int id)
         {
-            return VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+            if(id == 0) { 
+                return BadRequest(); 
+            }
+            var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound(); 
+            }
+            return Ok(villa);
 
+        }
+        [HttpPost]
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+            if(villaDto.Id > 0)
+            {
+                return BadRequest(StatusCodes.Status500InternalServerError);
+            }
+            villaDto.Id = VillaStore.VillaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.VillaList.Add(villaDto);
+            return Ok(villaDto);
         }
 
     }
